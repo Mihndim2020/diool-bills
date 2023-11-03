5d8863d08b0bfe5beefd236df614bd76d3f7bf7e
Mihndim Julius <mihndim2016@gmail.com>
2023-11-03 15:10:23 +0100

    Update generateToken.js

--- generateToken.js
+++ generateToken.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
-let url =  `https://auth.diool.com/oauth/token`;
+let url =  `/oauth/token`;
 
 let options   = {
     "method": "POST",
     "headers": {
         "Content-Type"  : "application/x-www-form-urlencoded"
     },
-    "body": `grant_type=client_credentials&client_id="71018ZtrizNnHHawXxeI1WfpBRXmb9Yl"&client_secret="MNPEaXN4DBql2LhMmKmiN5919qjWGvNuDxjEVc-RUeAPAMvYOCn6UfVUePNUsS7m"&audience="https://127.0.0.1:8080/diool/api/v1"&environment="PREPROD"&isPaymentLink=true`
+    "body": `grant_type=client_credentials&client_id=""&client_secret=""&audience="https://127.0.0.1:8080/diool/api/v1"&environment="PREPROD"&isPaymentLink=true`
 }
 
 const generateToken = async () =>  {
